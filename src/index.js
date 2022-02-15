4a1f71eb41173dcad44a415ca911b4294dd02d5d
Aliheydar Rustamov <alihaydarustamov92@gmail.com>
2022-02-15 15:31:18 +0400

    user page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import App from "./components/Base/App";
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,20 +8,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import configureStore from "./redux/reducers/configureStore";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/Base/App"
-import Login from "./components/Base/Login"
+import App from "./components/Base/App";
+import Login from "./components/Base/Login";
 const store = configureStore();
-
-
-
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-    <Routes>
-        <Route  path="/" element={<Login />} exact></Route>
-        <Route  exact path="/*" element={<App/>}></Route>
+      <Routes>
+        <Route path="/" element={<Login />} exact></Route>
+        <Route exact path="/*" element={<App />}></Route>
       </Routes>
       {/* <App/> */}
     </BrowserRouter>
