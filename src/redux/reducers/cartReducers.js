352fb60d089a479468274a98a1b147354fadc657
Aliheydar Rustamov <alihaydarustamov92@gmail.com>
2022-03-15 18:59:10 +0400

    some vhanges

--- src/redux/reducers/cartReducers.js
+++ src/redux/reducers/cartReducers.js
@@ -1,27 +1,27 @@
 import * as actionTypes from "../actions/actionTypes";
 import initialState from "./initialState";
 
-// export default function cartReducers(state = initialState, action) {
-//   console.log(action.payload);
-//   switch (action.type) {
-//     case actionTypes.SHOW_BASKET_ITEMS: {
+export default function cartReducers(state = initialState, action) {
+  console.log(action.payload);
+  switch (action.type) {
+    case actionTypes.SHOW_BASKET_ITEMS: {
 
-//       if (Object.entries(action.payload).length > 0) {
-//         var arr = [];
-//         for (let value of Object.values(action.payload.items)) {
-//           // setBasketDataState((oldArray) => [...oldArray, value]);
+      if (Object.entries(action.payload).length > 0) {
+        var arr = [];
+        for (let value of Object.values(action.payload.items)) {
+          // setBasketDataState((oldArray) => [...oldArray, value]);
          
-//           arr.push(value);
-//         }
-//       }
-//       return {
-//         ...state,
-//         addBasketItems: arr,
-//         basketAllData: action.payload,
-//       };
-//     }
+          arr.push(value);
+        }
+      }
+      return {
+        ...state,
+        addBasketItems: arr,
+        basketAllData: action.payload,
+      };
+    }
 
-//     default:
-//       return state;
-//   }
+    default:
+      return state;
+  }
 // }
