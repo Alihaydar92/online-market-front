9122b9ee0d55c5e0fbf8e21c4c26dba19ca37afa
Aliheydar Rustamov <alihaydarustamov92@gmail.com>
2022-03-15 19:03:09 +0400

    card reducer bug fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -7,7 +7,7 @@ import propertyReducers from "./propertyReducers";
 import storeHouseReducers from "./storeHouseReducers";
 import loaderReducers from "./loaderReducers";
 import authReducers from "./authReducers";
-// import cartReducers from "./cartReducers";
+import cartReducers from "./cartReducers";
 import blackListReducers from "./blackListReducers"
 const rootReducer = combineReducers({
   customerReducer,
@@ -18,7 +18,7 @@ const rootReducer = combineReducers({
   storeHouseReducers,
   loaderReducers,
   authReducers,
-  // cartReducers,
+  cartReducers,
   blackListReducers
 });
 
