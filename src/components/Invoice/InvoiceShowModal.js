caf218879fe39e76fc5ccd77f3a0081372eedb6b
Aliheydar Rustamov <alihaydarustamov92@gmail.com>
2022-04-18 14:54:30 +0400

    some changes

--- src/components/Invoice/InvoiceShowModal.js
+++ src/components/Invoice/InvoiceShowModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form, Input, Table } from "antd";
+import { Button, Form, Input, Table, Typography } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { exportPdf } from "../../redux/actions/invoiceActions";
 import jsPDF from "jspdf";
@@ -7,7 +7,7 @@ import "jspdf-autotable";
 import fontTxt from "../../helpers/fontRobotoBase64";
 import moment from "moment";
 const logo = require("../../helpers/greenStream.jpeg");
-
+const { Text } = Typography;
 export default function InvoiceShowModal(props) {
   const [form] = Form.useForm();
   const dispatch = useDispatch();
@@ -17,56 +17,88 @@ export default function InvoiceShowModal(props) {
 
   const invoicesByIdColumns = [
     {
+      title: "Barkod",
+      dataIndex: ["storeHouseDto", "barcode"],
+    },
+    {
+      title: "Adı",
+      dataIndex: ["storeHouseDto", "productDto", "name"],
+    },
+    {
       title: "Say",
       dataIndex: "quantity",
     },
     {
-      title: "İtirilmiş məbləğ",
-      dataIndex: "lostQuantity",
+      title: "Qiyməti",
+      dataIndex: ["storeHouseDto", "sellPrice"],
+    },
+    {
+      title: "Məbləğ",
+      dataIndex: "",
     },
     {
       title: "Güzəşt",
       dataIndex: "discountPercent",
     },
     {
-      title: "Güzəşt qiyməti",
+      title: "Güzəştli qiyməti",
       dataIndex: "discount",
     },
     {
       title: "Cəmi",
       dataIndex: "totalPrice",
     },
-    {
-      title: "Müştəri satış qiyməti",
-      dataIndex: "customerSellPrice",
-    },
-    {
-      title: "Digər qiymət",
-      dataIndex: "otherPrice",
-    },
-    {
-      title: "Qeyd",
-      dataIndex: "note",
-    },
   ];
 
   const pdfExport = () => {
-    dispatch(
-      exportPdf(
-        invoicesDataById,
-        props?.invoiceBaseDataProps
-      )
-    );
+    dispatch(exportPdf(invoicesDataById, props?.invoiceBaseDataProps));
   };
   return (
     <div>
       <Table
+      bordered
         // scroll={{ y: 530 }}
         // style={{ marginTop: "20px", wordBreak: "break-word" }}
         dataSource={invoicesDataById}
         columns={invoicesByIdColumns}
         rowKey="id"
         pagination={false}
+        summary={(pageData) => {
+          let totalBorrow = 0;
+
+          pageData.forEach(({ totalPrice }) => {
+            totalBorrow += totalPrice;
+          });
+
+          return (
+            <>
+              <Table.Summary.Row>
+                <Table.Summary.Cell>Total</Table.Summary.Cell>
+                <Table.Summary.Cell>
+                  
+                </Table.Summary.Cell>
+                <Table.Summary.Cell>
+                  
+                </Table.Summary.Cell>
+                <Table.Summary.Cell>
+                  
+                </Table.Summary.Cell>
+                <Table.Summary.Cell>
+                 
+                </Table.Summary.Cell>
+                <Table.Summary.Cell>
+                 
+                 </Table.Summary.Cell>
+                 <Table.Summary.Cell>
+                 
+                 </Table.Summary.Cell>
+                <Table.Summary.Cell>
+                  <Text type="danger">{totalBorrow}</Text>
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+            </>
+          );
+        }}
       ></Table>
       <Button
         type="primary"
